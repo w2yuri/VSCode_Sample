9734177a7eb8cf42a6fff7f2f15e021e304a004d
w2yuri <namatro3@gmail.com>
2024-05-24 22:47:02 +0900

    正しいFizzBuzz(ver.2)

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,27 +1,35 @@
-# `fizz_buzz`という名前のメソッドを定義。
-# 引数として`number`が受け取れる。
-# このメソッドは、与えられた`number`に基づいて結果を返す。
-def fizz_buzz(number)
-    # もし`number`が15で割り切れる場合、つまり`number`が3でも5でも割り切れる場合は、"FizzBuzz"を返す
-    if number % 15 == 0
-      "FizzBuzz"
-    #   もし`number`が3で割り切れる場合は、"Fizz"を返す
-    elsif number % 3 == 0
-      "Fizz"
-    #   もし`number`が5で割り切れる場合は、"Fizz"を返す
-    elsif number % 5 == 0
-      "Buzz"
+(1..100).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+        puts "FizzBuzz"
+    elsiif num % 3 == 0
+        puts "Fizz"
+    elsif num % 5 == 0
+        puts "Buzz"
     else
-        # 上記の条件に当てはまらない場合は、そのまま`number`を文字列に変換して返す
-      number.to_s
-    end
-  end
+        puts num
+    end 
+end 
+
+hennkouaruyo
+
+
+
+# def fizz_buzz(number)
+#     if number % 15 == 0
+#       "FizzBuzz"
+#     elsif number % 3 == 0
+#       "Fizz"
+#     elsif number % 5 == 0
+#       "Buzz"
+#     else
+#       number.to_s
+#     end
+#   end
   
-  puts "数字を入力してください。"
-  
-#   ユーザーからの入力を受け取り、整数に変換して`input`に代入
-  input = gets.to_i
-  
-  puts '結果は...'
-#   `fizz_buzz`メソッドに入力値`input`を渡して、その結果を表示。
-  puts fizz_buzz(input)
+# #   1から100までの範囲の数値に対して反復処理を行う。
+# #   それぞれの数値に対して、以下の処理が行われる。
+# # 　　　`fizz_buzz`メソッドに数値`number`を渡し、その結果を表示。
+# # 　　　　上記の処理を1から100までの数値に対して繰り返す。
+#   (1..100).each do |number|
+#     puts fizz_buzz(number)
+#   end 
